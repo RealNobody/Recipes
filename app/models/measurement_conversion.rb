47eb1333d697885f2d38133aaaf26f49af6e3e4e
RealNobody <Erik.Littell@Sage.com>
2013-03-04 12:59:01 -0700

    Fix bug with read-only records.

--- app/models/measurement_conversion.rb
+++ app/models/measurement_conversion.rb
@@ -5,7 +5,7 @@ class MeasurementConversion < ActiveRecord::Base
   belongs_to :larger_measuring_unit, class_name: "MeasuringUnit", foreign_key: :larger_measuring_unit_id
 
   default_scope joins("INNER JOIN measuring_units ON (measuring_units.id =" +
-                          "measurement_conversions.smaller_measuring_unit_id)").order("measuring_units.name, multiplier")
+                          "measurement_conversions.smaller_measuring_unit_id)").readonly(false).order("measuring_units.name, multiplier")
 
   validates :smaller_measuring_unit_id, presence: true
   validates :larger_measuring_unit_id, presence: true
